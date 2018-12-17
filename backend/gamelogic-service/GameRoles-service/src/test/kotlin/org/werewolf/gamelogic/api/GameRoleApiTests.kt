bfb0fb63d64db91a157cd2787c1c4f11409799c8
kleppa <Jarandwaagekleppa@gmail.com>
2018-12-17 19:52:12 +0100

    pagination working

--- backend/gamelogic-service/GameRoles-service/src/test/kotlin/org/werewolf/gamelogic/api/GameRoleApiTests.kt
+++ backend/gamelogic-service/GameRoles-service/src/test/kotlin/org/werewolf/gamelogic/api/GameRoleApiTests.kt
@@ -1,19 +1,24 @@
 package org.werewolf.gamelogic.api
 
+import com.werewolf.dtos.GameRoleDto
 import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.domain.Page
 import org.springframework.test.context.junit4.SpringRunner
 import org.werewolf.gamelogic.GameRoleTestBase
 
-class GameRoleApiTests: GameRoleTestBase() {
+class GameRoleApiTests : GameRoleTestBase() {
     @Test
-    fun basicTest(){
-        given().content(ContentType.JSON)
-                .get("/")
+    fun basicTest() {
+        val data = given()
+                .content(ContentType.JSON)
+                .get("/?page=2&size=5")
                 .then()
                 .statusCode(200)
+
+        data.extract().jsonPath().getList("data.content", GameRoleDto::class.java).forEach{ println(it.name)}
     }
 }
