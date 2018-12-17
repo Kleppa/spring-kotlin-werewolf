0858e5fcf9801366e1ae84779eb701c89696f555
Even Gabrielsen <gabrielsen.even@gmail.com>
2018-12-17 19:23:28 +0100

    Restructure modules

--- backend/services/roles/src/main/kotlin/org/werewolf/gamelogic/api/GameRolesApi.kt
+++ backend/services/roles/src/main/kotlin/org/werewolf/gamelogic/api/GameRolesApi.kt
@@ -8,7 +8,6 @@ import io.swagger.annotations.ApiResponse
 import io.swagger.annotations.ApiResponses
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
